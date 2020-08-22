713a26721e002fe4d477f7378f30b7c134e80036
James Lyne <jim+github@not-null.co.uk>
2020-08-22 01:46:25 +0100

    Remove unused exceptions

--- src/java/uk/co/notnull/proxydiscord/exceptions/AlreadyLinkedException.java
+++
@@ -1,7 +0,0 @@
-package uk.co.notnull.proxydiscord.exceptions;
-
-public class AlreadyLinkedException extends Exception {
-    public AlreadyLinkedException(String message) {
-        super(message);
-    }
-}
--- src/java/uk/co/notnull/proxydiscord/exceptions/InvalidTokenException.java
+++
@@ -1,7 +0,0 @@
-package uk.co.notnull.proxydiscord.exceptions;
-
-public class InvalidTokenException extends Exception {
-    public InvalidTokenException(String message) {
-        super(message);
-    }
-}
