f8bcb70abf17a586ed0d1a171cd0a4197184ecbb
James Lyne <jim+github@not-null.co.uk>
2022-09-07 15:10:44 +0100

    Refactor info command, add PlayerInfoEvent for modifying output

---
+++ ProxyDiscordAPI/src/main/java/uk/co/notnull/proxydiscord/api/info/PlayerInfo.java
@@ -0,0 +1,116 @@
+/*
+ * ProxyDiscord, a Velocity Discord bot
+ * Copyright (c) 2022 James Lyne
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package uk.co.notnull.proxydiscord.api.info;
+
+import com.velocitypowered.api.proxy.server.RegisteredServer;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@SuppressWarnings("unused")
+public class PlayerInfo {
+	private final UUID uuid;
+	private String username;
+	private boolean online;
+	private RegisteredServer currentServer = null;
+	private boolean vanished = false;
+	private QueueInfo queueInfo = null;
+	private final List<ExtraField> extraFields = new ArrayList<>();
+
+	public PlayerInfo(UUID uuid, String username) {
+		this.uuid = uuid;
+		this.username = username;
+	}
+
+	public record QueueInfo(@NotNull RegisteredServer server, int position) {
+		public QueueInfo {
+			java.util.Objects.requireNonNull(server);
+		}
+	}
+
+	public record ExtraField(@NotNull String name, @NotNull String value, boolean inline) {
+		public ExtraField {
+			java.util.Objects.requireNonNull(name);
+			java.util.Objects.requireNonNull(value);
+		}
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public UUID getUuid() {
+		return uuid;
+	}
+
+	public boolean isOnline() {
+		return online;
+	}
+
+	public RegisteredServer getCurrentServer() {
+		return currentServer;
+	}
+
+	public boolean isVanished() {
+		return vanished;
+	}
+
+	public QueueInfo getQueueInfo() {
+		return queueInfo;
+	}
+
+	public List<ExtraField> getExtraFields() {
+		return extraFields;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public void setOnline(boolean online) {
+		this.online = online;
+	}
+
+	public void setCurrentServer(RegisteredServer currentServer) {
+		this.currentServer = currentServer;
+	}
+
+	public void setVanished(boolean vanished) {
+		this.vanished = vanished;
+	}
+
+	public void setQueueInfo(QueueInfo queueInfo) {
+		this.queueInfo = queueInfo;
+	}
+
+	public void addExtraField(ExtraField field) {
+		extraFields.add(field);
+	}
+
+	public void addExtraFields(List<ExtraField> fields) {
+		extraFields.addAll(fields);
+	}
+}
--- src/java/uk/co/notnull/proxydiscord/bot/commands/Info.java
+++ src/java/uk/co/notnull/proxydiscord/bot/commands/Info.java
@@ -24,23 +24,29 @@
 package uk.co.notnull.proxydiscord.bot.commands;
 
 import com.velocitypowered.api.proxy.Player;
-import com.velocitypowered.api.proxy.server.RegisteredServer;
+import com.velocitypowered.api.proxy.ServerConnection;
 import net.luckperms.api.model.user.UserManager;
 import org.javacord.api.entity.message.embed.EmbedBuilder;
 import org.javacord.api.entity.permission.Role;
 import org.javacord.api.entity.user.User;
 import org.javacord.api.event.interaction.ApplicationCommandEvent;
+import org.javacord.api.event.interaction.AutocompleteCreateEvent;
 import org.javacord.api.event.interaction.SlashCommandCreateEvent;
 import org.javacord.api.event.interaction.UserContextMenuCommandEvent;
+import org.javacord.api.interaction.AutocompleteInteraction;
 import org.javacord.api.interaction.SlashCommandInteraction;
+import org.javacord.api.interaction.SlashCommandOptionChoice;
 import org.javacord.api.interaction.callback.InteractionImmediateResponseBuilder;
 import org.javacord.api.interaction.callback.InteractionOriginalResponseUpdater;
+import org.javacord.api.listener.interaction.AutocompleteCreateListener;
 import org.javacord.api.listener.interaction.SlashCommandCreateListener;
 import org.javacord.api.listener.interaction.UserContextMenuCommandListener;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import uk.co.notnull.proxydiscord.Messages;
+import uk.co.notnull.proxydiscord.Util;
 import uk.co.notnull.proxydiscord.api.VerificationResult;
+import uk.co.notnull.proxydiscord.api.events.PlayerInfoEvent;
+import uk.co.notnull.proxydiscord.api.info.PlayerInfo;
 import uk.co.notnull.proxydiscord.manager.LinkingManager;
 import uk.co.notnull.proxydiscord.ProxyDiscord;
 import uk.co.notnull.proxydiscord.manager.VerificationManager;
@@ -62,7 +68,6 @@ public class Info implements SlashCommandCreateListener, UserContextMenuCommandL
 	    this.verificationManager = ProxyDiscord.inst().getVerificationManager();
 	}
 
-    @Override
     public void onSlashCommandCreate(SlashCommandCreateEvent event) {
         SlashCommandInteraction interaction = event.getSlashCommandInteraction();
 
@@ -73,13 +78,11 @@ public class Info implements SlashCommandCreateListener, UserContextMenuCommandL
 		String subcommand = interaction.getOptions().get(0).getName();
 
 		switch (subcommand) {
-			case "discord" -> {
-				handleDiscordInfoRequest(interaction.getArguments().get(0).getUserValue().get(), event);
-			}
+			case "discord" -> //noinspection OptionalGetWithoutIsPresent
+					handleDiscordInfoRequest(interaction.getArguments().get(0).getUserValue().get(), event);
 
-			case "player" -> {
-				handleMinecraftInfoRequest(interaction.getArguments().get(0).getStringValue().get(), event);
-			}
+			case "player" -> //noinspection OptionalGetWithoutIsPresent
+					handleMinecraftInfoRequest(interaction.getArguments().get(0).getStringValue().get(), event);
 
 			case "server" -> event.getInteraction()
 					.createImmediateResponder()
@@ -100,103 +103,34 @@ public class Info implements SlashCommandCreateListener, UserContextMenuCommandL
 					"embed-info-discord-not-linked",
 					Collections.singletonMap("discord", user.getMentionTag())));
 		} else {
-			respondWithPlayerInfo(event, uuid, user.getId());
+			respondWithPlayerInfo(event, uuid);
 		}
 	}
 
+	/**
+	 * Handles a command invocation requesting information for the given Minecraft username or UUID
+	 * @param usernameOrUUID - UUID or username to return information for
+	 * @param event - The command event to respond to
+	 */
 	private void handleMinecraftInfoRequest(String usernameOrUUID, ApplicationCommandEvent event) {
 		if(Util.isValidUUID(usernameOrUUID)) {
-			UUID uuid = UUID.fromString(usernameOrUUID);
-			respondWithPlayerInfo(event, uuid, linkingManager.getLinked(uuid));
+			respondWithPlayerInfo(event, UUID.fromString(usernameOrUUID));
 		} else {
 			userManager.lookupUniqueId(usernameOrUUID).thenAccept(uuid -> {
 				if(uuid == null) {
 					respondWithEmbed(event, Messages.getEmbed("embed-info-player-not-found"));
 				} else {
-					respondWithPlayerInfo(event, uuid, linkingManager.getLinked(uuid));
+					respondWithPlayerInfo(event, uuid);
 				}
 			});
 		}
 	}
 
-	private void respondWithPlayerInfo(@NotNull ApplicationCommandEvent event, @NotNull UUID uuid, @Nullable Long discordId) {
-		CompletableFuture<InteractionOriginalResponseUpdater> updaterFuture = event.getInteraction().respondLater();
-		CompletableFuture<net.luckperms.api.model.user.User> userFuture = userManager.loadUser(uuid);
-		CompletableFuture<User> discordFuture;
-
-		if(discordId != null) {
-			discordFuture = plugin.getDiscord().getApi().getUserById(discordId);
-		} else {
-			discordFuture = CompletableFuture.completedFuture(null);
-		}
-
-		CompletableFuture.allOf(updaterFuture, userFuture, discordFuture).thenApplyAsync((unused) -> {
-			InteractionOriginalResponseUpdater updater = updaterFuture.join();
-			net.luckperms.api.model.user.User user = userFuture.join();
-			User discordUser = discordFuture.join();
-
-			if(user.getUsername() == null) {
-				return updater.addEmbed(Messages.getEmbed("embed-info-player-not-found"));
-			}
-
-			Optional<Player> onlinePlayer = plugin.getProxy().getPlayer(uuid);
-			String status = Messages.get("info-status-offline");
-			VerificationResult verifyStatus = discordUser != null ?
-					verificationManager.checkVerificationStatus(discordUser.getId()) :
-					VerificationResult.NOT_LINKED;
-
-			if(onlinePlayer.isPresent()) {
-				if(onlinePlayer.get().getCurrentServer().isPresent()) {
-					RegisteredServer server = onlinePlayer.get().getCurrentServer().get().getServer();
-					status = Messages.get("info-status-online-server",
-										  Collections.singletonMap("<server>", server.getServerInfo().getName()));
-				} else {
-					status = Messages.get("info-status-online");
-				}
-			}
-
-			String access = switch(verifyStatus) {
-				case UNKNOWN, NOT_LINKED -> Messages.get("info-access-not-linked");
-				case LINKED_NOT_VERIFIED -> Messages.get("info-access-linked-missing-roles");
-				case VERIFIED -> {
-					String roles = verificationManager.getVerifiedRoles().stream()
-							.filter(role -> role.hasUser(discordUser))
-							.map(Role::getMentionTag).collect(Collectors.joining(", "));
-
-					yield Messages.get("info-access-linked-roles", Collections.singletonMap("<roles>", roles));
-				}
-				case BYPASSED -> Messages.get("info-access-linked-bypassed");
-				case NOT_REQUIRED -> Messages.get("info-access-not-configured");
-			};
-
-			String discord = discordUser != null ?
-					Messages.get("info-discord-linked",
-								 Collections.singletonMap("<discord>", discordUser.getMentionTag())) :
-					Messages.get("info-discord-not-linked");
-
-			Map<String, String> replacements = Map.of(
-					"minecraft", user.getUsername(),
-					"uuid", uuid.toString(),
-					"online_status", status,
-					"discord_status", discord,
-					"access", access
-			);
-
-			return updater
-					.addEmbed(Messages.getEmbed("embed-info", replacements))
-					.addComponents(Messages.getMessageActionRow("info-actions", replacements).build());
-		}).thenAccept(InteractionOriginalResponseUpdater::update).exceptionally(e -> {
-			plugin.getLogger().warn("Failed to respond to interaction");
-			e.printStackTrace();
-
-			if(updaterFuture.isDone()) {
-				updaterFuture.join().removeAllEmbeds().addEmbed(Messages.getEmbed("embed-info-error")).update();
-			}
-
-			return null;
-		});
-	}
-
+	/**
+	 * Sends a response to the given command event containing the given embed
+	 * @param event - The command event to respond to
+	 * @param embed - The embed to respond with
+	 */
 	private void respondWithEmbed(@NotNull ApplicationCommandEvent event, EmbedBuilder embed) {
 		InteractionImmediateResponseBuilder builder = event.getInteraction().createImmediateResponder();
 
@@ -208,5 +142,113 @@ public class Info implements SlashCommandCreateListener, UserContextMenuCommandL
 					builder.removeAllEmbeds().addEmbed(Messages.getEmbed("embed-info-error")).respond();
 					return null;
 				});
+	}
+
+	/**
+	 * Sends a response to the given command event containing player info for the given UUID.
+	 * If the given UUID is not known to the server, a "Player not found" response will be sent.
+	 * @param event - The command event to respond to
+	 * @param uuid - The UUID of the user to return information for
+	 */
+	private void respondWithPlayerInfo(@NotNull ApplicationCommandEvent event, @NotNull UUID uuid) {
+		CompletableFuture<InteractionOriginalResponseUpdater> updaterFuture = event.getInteraction().respondLater();
+		CompletableFuture<net.luckperms.api.model.user.User> userFuture = userManager.loadUser(uuid);
+		CompletableFuture<User> discordFuture;
+		Long discordId = linkingManager.getLinked(uuid);
+
+		if (discordId != null) {
+			discordFuture = plugin.getDiscord().getApi().getUserById(discordId);
+		} else {
+			discordFuture = CompletableFuture.completedFuture(null);
+		}
+
+		CompletableFuture.allOf(updaterFuture, userFuture, discordFuture).thenCompose((unused) -> {
+			net.luckperms.api.model.user.User user = userFuture.join();
+
+			if (user.getUsername() == null) {
+				return null;
+			}
+
+			PlayerInfo playerInfo = new PlayerInfo(uuid, user.getUsername());
+			Optional<Player> onlinePlayer = plugin.getProxy().getPlayer(uuid);
+
+			if (onlinePlayer.isPresent()) {
+				playerInfo.setOnline(true);
+				playerInfo.setCurrentServer(onlinePlayer.get().getCurrentServer()
+													.map(ServerConnection::getServer)
+													.orElse(null));
+			}
+
+			return plugin.getProxy().getEventManager().fire(new PlayerInfoEvent(playerInfo));
+		}).thenApply(result -> {
+			InteractionOriginalResponseUpdater updater = updaterFuture.join();
+
+			if (result == null) {
+				return updater.addEmbed(Messages.getEmbed("embed-info-player-not-found"));
+			}
+
+			return preparePlayerInfoResponse(updater, result.getPlayerInfo(), discordFuture.join());
+		}).thenAccept(InteractionOriginalResponseUpdater::update).exceptionally(e -> {
+			plugin.getLogger().warn("Failed to respond to interaction");
+			e.printStackTrace();
+
+			if (updaterFuture.isDone()) {
+				updaterFuture.join().removeAllEmbeds().addEmbed(Messages.getEmbed("embed-info-error")).update();
+			}
+
+			return null;
+		});
+	}
+
+	private InteractionOriginalResponseUpdater preparePlayerInfoResponse(
+			InteractionOriginalResponseUpdater updater, PlayerInfo info, User discordUser) {
+		String discord = discordUser != null ?
+				Messages.get("info-discord-linked",
+							 Collections.singletonMap("<discord>", discordUser.getMentionTag())) :
+				Messages.get("info-discord-not-linked");
+
+		String status = Messages.get("info-status-offline");
+		VerificationResult verifyStatus = discordUser != null ?
+				verificationManager.checkVerificationStatus(discordUser.getId()) :
+				VerificationResult.NOT_LINKED;
+
+		String access = switch (verifyStatus) {
+			case UNKNOWN, NOT_LINKED -> Messages.get("info-access-not-linked");
+			case LINKED_NOT_VERIFIED -> Messages.get("info-access-linked-missing-roles");
+			case VERIFIED -> {
+				String roles = verificationManager.getVerifiedRoles().stream()
+						.filter(role -> role.hasUser(discordUser))
+						.map(Role::getMentionTag).collect(Collectors.joining(", "));
+
+				yield Messages.get("info-access-linked-roles", Collections.singletonMap("<roles>", roles));
+			}
+			case BYPASSED -> Messages.get("info-access-linked-bypassed");
+			case NOT_REQUIRED -> Messages.get("info-access-not-configured");
+		};
+
+		if (info.isOnline() && !info.isVanished()) {
+			if(info.getQueueInfo() != null) {
+				status = Messages.get("info-status-queueing", Map.of(
+						"<server>", info.getQueueInfo().server().getServerInfo().getName(),
+						"<position>", String.valueOf(info.getQueueInfo().position())));
+			} else if (info.getCurrentServer() != null) {
+				status = Messages.get("info-status-online-server", Collections.singletonMap(
+						"<server>", info.getCurrentServer().getServerInfo().getName()));
+			} else {
+				status = Messages.get("info-status-online");
+			}
+		}
+
+		Map<String, String> replacements = Map.of(
+				"minecraft", info.getUsername(),
+				"uuid", info.getUuid().toString(),
+				"online_status", status,
+				"discord_status", discord,
+				"access", access
+		);
+
+		return updater
+				.addEmbed(Messages.getEmbed("embed-info", replacements))
+				.addComponents(Messages.getMessageActionRow("info-actions", replacements).build());
 	}
 }
